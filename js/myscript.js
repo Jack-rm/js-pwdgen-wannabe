1f21d6cc8df1ef817a5b1ee990f5f052ec28eb90
Jack-rm <jackcnsr94@gmail.com>
2021-08-04 15:47:31 +0200

    "undoutbly insecure" Password generator exercise

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+var name = prompt("Insert your name");
+var surname = prompt("Insert your surname")
+var color = prompt("Insert your favourite color")
+
+document.getElementById("password").innerHTML = name + surname + color + 21;
